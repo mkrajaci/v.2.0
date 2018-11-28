d6d4e8d96d04b1ea293cb4c906c0d704552d46e9
Mario <krajacic.mario@gmail.com>
2018-11-28 07:17:06 +0100

    Building

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return '<h1>Hello jedan!</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About jedan!</h1>'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
