9d86498615edf35faefc463622794fcf4f9b003d
Mario <krajacic.mario@gmail.com>
2018-11-29 07:09:28 +0100

    Adding Bootstrap

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Mario',
+        'title': 'Prvi blog',
+        'content': 'Prvi sadrzaj',
+        'date_posted': '28.11.2018.'
+    },
+    {
+        'author': 'Marija',
+        'title': 'Drugi blog',
+        'content': 'Drugi sadrzaj',
+        'date_posted': '29.11.2018.'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
-def home():
-    return '<h1>Hello jedan!</h1>'
+def home():                             # posts koristim za referencu u htmlu
+    return render_template('home.html', posts=posts) # mjesto gdje odredjujem koji html rederira
 
 @app.route('/about')
 def about():
-    return '<h1>About jedan!</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
