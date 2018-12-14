f1ea1b2301b31a0500dfc9d77e67e98e637453ed
Mario <krajacic.mario@gmail.com>
2018-12-14 07:13:14 +0100

    Adding environ variables

--- blog/config.py
+++ blog/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
